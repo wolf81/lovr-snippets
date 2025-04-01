eb0cce76bd9891e3e6f2e8b9553e2bc7dfe90ad9
Wolfgang Schreurs <wolfgang.schreurs@gmail.com>
2025-04-01 09:41:03 +0700

    Code clean-up

--- load-bsp/main.lua
+++ load-bsp/main.lua
@@ -1,7 +1,17 @@
+lovr.mouse = require 'lovr-mouse'
+
 local Pak = require 'pak'
 
 local meshes = {}
 local textures = {}
+
+local camera = {
+    transform = lovr.math.newMat4(),
+    position = lovr.math.newVec3(),
+    movespeed = 200,
+    pitch = 0,
+    yaw = 0,
+}    
 
 function lovr.load()
     local pak = Pak('pak0.pak')
@@ -68,16 +78,56 @@ function lovr.load()
             end
         end
     end)
+
+    lovr.mouse.setRelativeMode(true)
 end
 
 function lovr.update(dt)
+    local velocity = vec4()
 
+    if lovr.system.isKeyDown('w', 'up') then
+        velocity.z = -1
+    elseif lovr.system.isKeyDown('s', 'down') then
+        velocity.z = 1
+    end
+
+    if lovr.system.isKeyDown('a', 'left') then
+        velocity.x = -1
+    elseif lovr.system.isKeyDown('d', 'right') then
+        velocity.x = 1
+    end
+
+    if #velocity > 0 then
+        velocity:normalize()
+        velocity:mul(camera.movespeed * dt)
+        camera.position:add(camera.transform:mul(velocity).xyz)
+    end
+
+    camera.transform:identity()
+    camera.transform:translate(0, 1.7, 0)
+    camera.transform:translate(camera.position)
+    camera.transform:rotate(camera.yaw, 0, 1, 0)
+    camera.transform:rotate(camera.pitch, 1, 0, 0)
 end
 
 function lovr.draw(pass)
-    -- pass:setShader('normal')
-    -- pass:setWireframe(true)
+    pass:push()
+    pass:setViewPose(1, camera.transform)
+    pass:setColor(0xffffff)
     for i, mesh in ipairs(meshes) do
         pass:draw(mesh)
+    end
+    pass:pop()    
+end
+
+function lovr.mousemoved(x, y, dx, dy)
+    camera.pitch = camera.pitch - dy * .004
+    camera.yaw = camera.yaw - dx * .004
+end
+
+
+function lovr.keypressed(key)
+    if key == 'escape' then
+        lovr.event.quit()
     end
 end
